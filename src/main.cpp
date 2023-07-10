760198a4b3dc334ffe8cdfabc3b369407337275f
cristovaoeustaquio <67655102+cristovaoeustaquio@users.noreply.github.com>
2023-07-10 16:38:33 -0300

    mqtt funcionando

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,13 @@
 #include<WiFi.h>
 #include <PubSubClient.h>
 #define AOUT_PIN 36 // ESP32 pin GIOP36 (ADC0) that connects to AOUT pin of moisture sensor
+#define PUMP_PIN 19 // ESP32 pin GPIO19 that connects to pump driver circuit
+#define SAMPLES 10
+#define DRY 1
+#define WET 2
+#define WATER 3
+#define LIMITE_SECO 30
+#define LIMITE_UMIDO 60
 
 const char* ssid = "ClaroWiFi";       // SSID / nome da rede WiFi
 const char* password = "yuri5252";  // Senha da rede WiFi
@@ -10,16 +17,18 @@ const char* mqttServer = "test.mosquitto.org"; // Endereço IP do Mosquitto Brok
 const int mqttPort = 1883;                // Porta do Mosquitto Broker
 int long cont = 0;
 
-const char* topico = "umidade";
+const char* topico = "mslc/umidade";
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
+
+bool pump_state = false;
 
 float calibracao(int sensor_value)
 {
   int sensor_value_real = 0;
   float calc = 0;
-  if (sensor_value > 2790){
-    sensor_value_real = 2790;
+  if (sensor_value > 2700){
+    sensor_value_real = 2700;
     return calc;
   } else sensor_value_real = sensor_value;
   calc = 77.553 - 0.033557 * sqrt(2980 * sensor_value_real - 2703490);
@@ -50,7 +59,7 @@ void reconnectMQTT() {
     Serial.print("Conectando-se ao broker MQTT: ");
     Serial.println(mqttServer);
 
-    if (mqttClient.connect("ESP8266Client")) {
+    if (mqttClient.connect("mslcClient")) {
       Serial.println("Conectado ao broker MQTT com sucesso!");
     } else {
       Serial.print("Falha na conexão ao broker. Estado: ");
@@ -61,33 +70,89 @@ void reconnectMQTT() {
   }
 }
 
+
+float realizar_leitura_sensor(){
+  int value = analogRead(AOUT_PIN); // read the analog value from sensor
+  float valor_calibrado = calibracao(value);
+  return valor_calibrado;
+}
+
+float ler_umidade(){
+  float soma = 0;
+  for(int i = 0; i < SAMPLES; i++){
+    soma += realizar_leitura_sensor();
+    delay(20);
+  }
+  return soma/SAMPLES;
+}
+
+int classificar_umidade(float umidade){
+  if(umidade <= LIMITE_SECO){
+    return DRY;
+  }
+  else if(umidade <= LIMITE_UMIDO){
+    return WET;
+  }
+  else{
+    return WATER;
+  }
+}
+
+
+bool bomba_esta_desligada(){
+  return !pump_state;
+}
+
+bool bomba_esta_ligada(){
+  return pump_state;
+}
+
+void desativar_bomba(){
+  if(bomba_esta_ligada()){
+    digitalWrite(PUMP_PIN, LOW);
+    pump_state = false;
+  }
+}
+
+void ativar_bomba(){
+  Serial.println(pump_state);
+  if(bomba_esta_desligada()){
+    digitalWrite(PUMP_PIN, HIGH);
+    pump_state = true;
+  }
+}
+
+
+void comandar_bomba(int classificacao){
+  if(classificacao == DRY){
+    ativar_bomba();
+  }
+  else{
+    desativar_bomba();
+  }
+}
+
+
 void setup() {
   Serial.begin(9600);
   setupWiFi();
   mqttClient.setServer(mqttServer, mqttPort);
+  pinMode(PUMP_PIN, OUTPUT);    // sets the digital pin 19 as output
 }
 
 void loop() {
-  int value = analogRead(AOUT_PIN); // read the analog value from sensor
-  float valor_calibrado = calibracao(value);
+  float valor_calibrado = ler_umidade();
+  int classif = classificar_umidade(valor_calibrado);
+  comandar_bomba(classif);
+  
   Serial.print("Calibracao:");
   Serial.println(valor_calibrado);
 
-  Serial.print("Moisture value: ");
-  Serial.println(value);
-
   delay(500);
-  if (!mqttClient.connected()) {
-    reconnectMQTT();
+  if (mqttClient.connected()) {
+    mqttClient.publish(topico, String(valor_calibrado).c_str(), 1);
+    mqttClient.loop();
   }
-  mqttClient.loop();
-
-  // Publica uma mensagem no tópico "Umidade"
-  mqttClient.publish(topico, String(valor_calibrado).c_str(), 1);
-
-  cont = cont + 1;
-  Serial.print("Hello, MQTT! ");
-  Serial.print(cont);
-  Serial.print("\n");
-  delay(300);
+  else reconnectMQTT();
+  
 }
